00c16d4bbdf20bc6b89a169b3da8d0e7df06f57b
Shander Andrade <s.hander8910@hotmail.com>
2022-04-16 14:02:37 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { rastrearEncomendas } = require('correios-brasil');
+const codigosJson = require('../codigos.json');
+
+let codRastreio = [];
+
+codigosJson.forEach(codigo => {
+    codRastreio.push(codigo.codigo);
+});
+
+rastrearEncomendas(codRastreio)
+    .then((encomendas) => {
+        encomendas.forEach((encomenda, index) => {
+            console.log(codigosJson[index].itemNome);
+            console.table(encomenda);
+            console.log('============================================================================================================================================================\n\n');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
